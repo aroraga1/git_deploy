40a1031fef3d1d7d886be1f0da0550d14c0c39bc
garvit arora <aroragarvit1805@gmail.com>
2023-05-18 03:06:15 +0530

    first commit

---
+++ hellow.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    printf("hellow");
+  int a,i ;
+   printf("enter a no,of choice for print a table");
+   scanf("%d",&a);
+   for ( i = 0; i <n; i++)
+   {
+    /* code */
+   }
+   
+   a=a*i;
+   
+   printf("the dable of a is :%d",a);
+    printf("the sum of this table is:%d\n",g);
+
+return 0; 
+} 
